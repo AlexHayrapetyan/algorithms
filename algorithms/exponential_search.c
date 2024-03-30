97188e509d9633c8572f3210acc5c7c8cd012d46
AlexHayrapetyan <118679950+AlexHayrapetyan@users.noreply.github.com>
2024-03-30 12:29:02 +0400

    Update exponential_search.c

--- algorithms/exponential_search.c
+++ algorithms/exponential_search.c
@@ -24,6 +24,7 @@ int binarySearch(int arr[], int l, int r, int x)
 		int mid = l + (r - l)/2;
 		if (arr[mid] == x){
 			return mid;
+	
         }
 
 		if (arr[mid] > x){
@@ -37,11 +38,11 @@ int binarySearch(int arr[], int l, int r, int x)
 
 int main(void)
 {
-int arr[] = {2, 3, 4, 10, 40};
-int n = sizeof(arr)/ sizeof(arr[0]);
-int x = 10;
-int result = exponentialSearch(arr, n, x);
-(result == -1)? printf("Element is not present in array")
-: printf("Element is present at index %d",result);
-return 0;
+	int arr[] = {2, 3, 4, 10, 40};
+	int n = sizeof(arr)/ sizeof(arr[0]);
+	int x = 10;
+	int result = exponentialSearch(arr, n, x);
+	(result == -1)? printf("Element is not present in array")
+	: printf("Element is present at index %d",result);
+	return 0;
 }
